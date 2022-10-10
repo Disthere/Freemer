8fa782c6a77f15dbb4fa2cd381502100cf9d076b
Вячеслав Литвиненко <ptb.ikc@yandex.ru>
2022-10-10 17:44:29 +0500

    Добавьте файлы проекта.

---
+++ Freemer cons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Freemer_cons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
